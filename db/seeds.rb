7277c57d5d026a80ce274ba5fd0b792f0c12be54
Uzair <uzair.anwer12@gmail.com>
2022-10-25 12:18:05 +0500

    seed updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+# u = User.create([{name: "seed"},{name: "seeding"}])
+10.times do |i|
+    p = Post.create!({title:"Ghayoor #{i}",body:"seedingof10", status:"public",user_id: "3"})
+   
+end
+
+p "Created #{Post.count} posts"
